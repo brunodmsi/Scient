b8ae0acbc327f4ba3901bd86f862959fc2b38635
Bruno De Masi <brunodemasi1@gmail.com>
2020-04-20 04:50:49 -0300

    fix: fixed incoming/current changes

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const { resolve } = require('path');
-
-const app = express();
-
-const builded = resolve(__dirname, 'build', 'index.html');
-app.get('*', (request, response) => response.sendFile(builded));
-
-app.listen(3000);
-=======
 const express = require('express')
 
 const app = express()
@@ -22,4 +11,3 @@ app.get('*', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 const port = process.env.REACT_APP_DEPLOY_PORT || 4000;
 
 app.listen(port, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
->>>>>>> b98c610edd689b8a03f50814a74b7dd65fbc1ef5
